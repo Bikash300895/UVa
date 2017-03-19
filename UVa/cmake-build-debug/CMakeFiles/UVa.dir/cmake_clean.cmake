4c137950a3b0a912b419d95b5e0c6854d885bb85
Bikash <bikash11roy@gmail.com>
2017-03-19 13:25:11 +0600

    uva test project
    Signed-off-by: Bikash <bikash11roy@gmail.com>

---
+++ UVa/cmake-build-debug/CMakeFiles/UVa.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UVa.dir/main.cpp.obj"
+  "UVa.pdb"
+  "UVa.exe"
+  "UVa.exe.manifest"
+  "libUVa.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UVa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
